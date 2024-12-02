2ac60281f7a2ac4bfa54e1b11a0212fd7a64da50
kawsarhossen1 <kawsarhossen66@gmail.com>
2024-12-02 11:08:05 +0600

    home js and home html file added

--- JS/login.js
+++ JS/login.js
@@ -5,8 +5,21 @@ document
     // step 2 browser load houa off
     event.preventDefault();
 
-    // step 3 get the phn number
+    // step 3 get the phn number and the pin number
 
     const phoneNumber = document.getElementById("phone-number").value;
-    console.log(phoneNumber);
+    const pinNumber = document.getElementById("pin-number").value;
+
+    console.log(phoneNumber, pinNumber);
+
+    // validate phone and pin
+    // this is not current way to check
+    if (phoneNumber === "5" && pinNumber === "1234") {
+      console.log("you are logged in");
+      window.location.href = '/home.html';
+
+      //   allow user to use the website
+    } else {
+      alert("Wrong phone number or pin");
+    }
   });
