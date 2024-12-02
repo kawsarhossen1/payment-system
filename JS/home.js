70571d5539774f653cb4891b6c6ebfa19bf99961
kawsarhossen1 <kawsarhossen66@gmail.com>
2024-12-02 17:04:24 +0600

    update current balance

--- JS/home.js
+++ JS/home.js
@@ -8,4 +8,21 @@ document
     const addMoneyInput = document.getElementById("input-add-money").value;
     const pinNumberInput = document.getElementById("pin-number-input").value;
     console.log(addMoneyInput, pinNumberInput);
+
+    // not right way
+    if (pinNumberInput === "1234") {
+      console.log("Adding money to your account");
+
+      //   get the current balance
+      const balance = document.getElementById("account-balance").innerText;
+      const addNewNumber = parseFloat(addMoneyInput);
+      const balanceNumber = parseFloat(balance);
+      const newBalance = addNewNumber + balanceNumber;
+      console.log(newBalance);
+
+        // update current balance
+      document.getElementById('account-balance').innerText = newBalance;
+    } else {
+      alert("Failed to add money! Please try again.");
+    }
   });
