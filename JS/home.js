d1edc88441ebe828e5161b7970df98581a6f9cc8
kawsarhossen1 <kawsarhossen66@gmail.com>
2024-12-02 14:42:05 +0600

    add money button added

--- JS/home.js
+++ JS/home.js
@@ -1,1 +1,11 @@
-console.log('home');
+// add money to the account
+
+document
+  .getElementById("add-money-btn")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+
+    const addMoneyInput = document.getElementById("input-add-money").value;
+    const pinNumberInput = document.getElementById("pin-number-input").value;
+    console.log(addMoneyInput, pinNumberInput);
+  });
